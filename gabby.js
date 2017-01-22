58de42f12d32ef21b3ea9dbff20896b6063b2434
Richa Mehta <purplestencil@gmail.com>
2017-01-22 16:50:44 -0500

    First pass complete. Gabbycat chats!

--- gabby.js
+++ gabby.js
@@ -1,38 +1,91 @@
+//Gabbycat's vocabulary
+var gabbyVocab = ["purr purr purr!", "chase the stick?", "hide under covers.", "big eyes", "tail swish", "snuggle", "cuddle", "sniff", "scratch the carpet", "watch the birds", "mrrraow!", "hiss", "paw your face", "whisker tickles", "where's the laser?", "meow", "shed everywhere", "land on my feet", "make mummy laugh", "sit on daddy's lap", "wash wash whaa-?", "butthole must sparkle", "love my humans", "where's my food?", "bat bat bat", "ignore you", "look away", "staring contest", "knead the blankie", "I fits, I sits!";]
+
 function gabbycatIpsum(elemID) {
 
-  var gabbyVocab = ["purr purr purr!", "chase the stick ", "hide under blankie.", "big eyes ", "tail swish "];
+  //set minimum and maximum phrase count
+  //I'll have to come back and revisit so that 
+  //users can set the number of words exactly
+  var minWord = 10; //document.getElementById('min-word').value;
+  var maxWord = 30; //document.getElementById('max-word').value;
 
-  var minWord = 5; //document.getElementById('min-word').value;
-  var maxWord = 10; //document.getElementById('max-word').value;
-  var numPara = 3; //document.getElementById('num-para').value;
-  var wordCount = Math.floor(Math.random() * (maxWord - minWord)) + minWord;
-
+  //set text variables
   var gabbyText = new Array();
   var paraText = "";
 
+  //get div element
+  var gabbyCat = document.getElementById(elemID);
+
+  //get number of paragraphs user input
+  var numPara = document.getElementById('num-para').value;
+
+  /*if (numPara == 0) {
+    gabbyCat.innerHTML = "Gabby needs to know how much to gab!";
+  }*/
+
+  //create gabby-div to hold gabbycat ipsum text
+  //var gabbyCat = document.createElement("gabby-div");
+  //gabbyCat.class = "well center-block";
+  //document.getElementById("gab").appendChild(gabbyCat);
+
+  //default first sentence
+  gabbyCat.innerHTML = "Gabby ipsum gabby cat chat.";
+
   for (var i = 0; i < numPara; i++) {
+    //generate random count for each paragraph
+    var wordCount = Math.floor(Math.random() * (maxWord - minWord)) + minWord;
     for (var j = 0; j < wordCount; j++) {
+      //pick a random phrase from vocab array
       var newPhrase = gabbyVocab[Math.floor(Math.random() * (gabbyVocab.length - 1))];
+      //select last character of current phrase/para
       var lastChar = paraText.substring(paraText.length - 1, paraText.length);
-      if (lastChar == "." || lastChar == "?" || lastChar == "!") {
-        newPhrase = newPhrase.substring(0, 1).toUpperCase() + newPhrase.substring(1, newPhrase.length);
+      //check if last char is terminal punctuation
+      if (lastChar == "." || lastChar == "?" || lastChar == "!" || lastChar == "") {
+        //if yes, capitalize next letter
+        newPhrase = " " + newPhrase.substring(0, 1).toUpperCase() + newPhrase.substring(1, newPhrase.length);
+      } else {
+        //if no, just add a space and move on
+        newPhrase = " " + newPhrase;
       }
-      paraText += " " + newPhrase;
+      //append new phrase to the current paragraph
+      paraText += newPhrase;
     }
-    gabbyText[i] = paraText.substring(0, 1).toUpperCase() + paraText.substring(1, paraText.length);
-  }
-  gabbyText;
-  
-  var gabbyCat =  document.getElementById(elemID);
-
-  gabbyCat.innerHTML = gabbyCat.innerHTML + "Gabby ipsum " + gabbyText[0].substring(0,1).toLowerCase() + gabbyText[0].substring(1, gabbyText[0].length - 1) + ".</p>";
-  
-  for (var k = 1; k < numPara; k++) {
-    gabbyCat.innerHTML = gabbyCat.innerHTML + gabbyText[k].substring(0, gabbyText[k].length - 1) + ".</p>";
+    //select last char of paragraph
+    var lastChar = paraText.substring(paraText.length - 1, paraText.length);
+    //if last char is a letter, append a period
+    if (/[a-zA-Z]/.test(lastChar)) {
+      gabbyText[i] = paraText + ".</p>";
+    } else {
+      gabbyText[i] = paraText + "</p>";
+    }
+    //add paragraph to generated text
+    gabbyCat.innerHTML += gabbyText[i];
   }
 }
 
-function clearElem(elemID) {
-  document.getElementById(elemID).innerHTML = "";
+//clears the generated text 
+function clearElem() {
+  document.getElementById('gabby-div').innerHTML = "Gabbycat text will appear here";
+  document.getElementById('copystatus').innerHTML = "";
 }
 
+//copies generated text to clipboard
+//this code was from a kind soul on stackoverflow
+//need to revisit and see how it actually works 
+//and if I can alter it for my purposes
+function copyToClip(elemID) {
+
+  if (document.selection) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById(elemID));
+    range.select().createTextRange();
+    document.execCommand("Copy");
+
+  } else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById(elemID));
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+  }
+  document.getElementById('copystatus').innerHTML = "Copied to clipboard!";
+}
